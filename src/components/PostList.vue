3e043fe1ae6f7aba8b2afd9263437464833cd47e
Ian Travers <mr.itpe@gmail.com>
2022-07-08 09:53:17 +0300

    Creating Base Component in A Vue Application (AppDate.vue) (ep.25)

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -24,22 +24,16 @@
         </div>
       </div>
 
-      <div class="post-date text-faded" :title="humanFriendlyDate(post.publishedAt)">
-        {{ diffForHumans(post.publishedAt) }}
+      <div class="post-date text-faded">
+        <AppDate :timestamp="post.publishedAt"/>
       </div>
-
     </div>
-
   </div>
 </template>
 
 <script>
 import sourceData from '@/data.json'
-import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
-import localizedDate from 'dayjs/plugin/localizedFormat'
-dayjs.extend(relativeTime)
-dayjs.extend(localizedDate)
+import AppDate from '@/components/AppDate'
 
 export default {
   name: 'PostList',
@@ -49,6 +43,9 @@ export default {
       required: true
     }
   },
+  components: {
+    AppDate
+  },
   data () {
     return {
       users: sourceData.users
@@ -57,12 +54,6 @@ export default {
   methods: {
     userById (userId) {
       return this.users.find(u => u.id === userId)
-    },
-    diffForHumans (timestamp) {
-      return dayjs.unix(timestamp).fromNow()
-    },
-    humanFriendlyDate (timestamp) {
-      return dayjs.unix(timestamp).format('llll')
     }
   }
 }
--- src/components/ThreadList.vue
+++ src/components/ThreadList.vue
@@ -9,7 +9,7 @@
             <router-link :to="{name: 'ThreadShow', params: {id: thread.id}}">{{ thread.title }}</router-link>
           </p>
           <p class="text-faded text-xsmall">
-            By <a href="#">{{ userById(thread.userId).name }}</a>, {{ thread.publishedAt }}.
+            By <a href="#">{{ userById(thread.userId).name }}</a>, <AppDate :timestamp="thread.publishedAt"/>.
           </p>
         </div>
 
@@ -24,7 +24,9 @@
             <p class="text-xsmall">
               <a href="#">{{ userById(thread.userId).name }}</a>
             </p>
-            <p class="text-xsmall text-faded">{{ thread.publishedAt }}</p>
+            <p class="text-xsmall text-faded">
+              <AppDate :timestamp="thread.publishedAt"/>
+            </p>
           </div>
         </div>
       </div>
@@ -34,6 +36,7 @@
 
 <script>
 import sourceData from '@/data.json'
+import AppDate from '@/components/AppDate'
 
 export default {
   props: {
@@ -42,14 +45,15 @@ export default {
       required: true
     }
   },
-
+  components: {
+    AppDate
+  },
   data () {
     return {
       posts: sourceData.posts,
       users: sourceData.users
     }
   },
-
   methods: {
     postById (postId) {
       return this.posts.find(p => p.id === postId)
