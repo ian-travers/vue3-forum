cddca1d3396f299b28244baec5be7b79b2547a2d
Ian Travers <mr.itpe@gmail.com>
2022-07-08 10:02:18 +0300

    Register Base Component Globally (ep.26)

--- src/components/ThreadList.vue
+++ src/components/ThreadList.vue
@@ -36,7 +36,6 @@
 
 <script>
 import sourceData from '@/data.json'
-import AppDate from '@/components/AppDate'
 
 export default {
   props: {
@@ -44,9 +43,6 @@ export default {
       type: Array,
       required: true
     }
-  },
-  components: {
-    AppDate
   },
   data () {
     return {
