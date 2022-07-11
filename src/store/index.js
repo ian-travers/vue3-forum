4f2c9594f9c09164c6ef88a8f1194ea2e86c19d7
Ian Travers <mr.itpe@gmail.com>
2022-07-11 11:45:59 +0300

    Fetching the Authenticated User From Vuex (VI) (ep. 33)

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,13 @@ import { createStore } from 'vuex'
 import sourceData from '@/data.json'
 
 export default createStore({
-  state: sourceData,
+  state: {
+    ...sourceData,
+    authId: 'f5xvKdIPQdSrUtT6i3UiHYttRXO2'
+  },
+  getters: {
+    authUser: state => state.users.find(user => user.id === state.authId)
+  },
   actions: {
     createPost (context, post) {
       post.id = 'ggg' + Math.random()
