4e7c3d7da0d1e663dee4f8d14fe9a5aa17822db8
Ian Travers <mr.itpe@gmail.com>
2022-07-12 10:23:17 +0300

    Sharing Components Between Multiple Pages (ep. 38)

--- src/components/UserProfileCardEditor.vue
+++ src/components/UserProfileCardEditor.vue
@@ -75,7 +75,7 @@
       </div>
 
       <div class="btn-group space-between">
-        <button class="btn-ghost">Cancel</button>
+        <button class="btn-ghost" @click="cancel">Cancel</button>
         <button class="btn-blue" type="submit">Save</button>
       </div>
     </form>
@@ -99,6 +99,10 @@ export default {
   methods: {
     save () {
       this.$store.dispatch('updateUser', { ...this.activeUser })
+      this.$router.push({ name: 'Profile' })
+    },
+    cancel () {
+      this.$router.push({ name: 'Profile' })
     }
   }
 }
--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -2,18 +2,8 @@
   <div class="container">
     <div class="flex-grid">
       <div class="col-3 push-top">
-        <UserProfileCard :user="user"/>
-        <UserProfileCardEditor :user="user"/>
-        <p class="text-xsmall text-faded text-center">
-          Member since june 2003, last visited 4 hours ago
-        </p>
-
-        <div class="text-center">
-          <hr />
-          <a href="edit-profile.html" class="btn-green btn-small"
-          >Edit Profile</a
-          >
-        </div>
+        <UserProfileCardEditor v-if="edit" :user="user"/>
+        <UserProfileCard v-else :user="user"/>
       </div>
 
       <div class="col-7 push-top">
@@ -36,6 +26,12 @@ import { mapGetters } from 'vuex'
 
 export default {
   name: 'PageProfile',
+  props: {
+    edit: {
+      type: Boolean,
+      default: false
+    }
+  },
   components: { UserProfileCard, UserProfileCardEditor, PostList },
   computed: {
     ...mapGetters({ user: 'authUser' }),
