c40ca1780643b53956396a108bbee71a2445eee9
Ian Travers <mr.itpe@gmail.com>
2022-07-13 16:35:43 +0300

    Fixing Replies && Threads Count  on the PostList (ep. 52)

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -12,7 +12,8 @@
           <img class="avatar-large" :src="userById(post.userId).avatar" alt="">
         </a>
 
-        <p class="desktop-only text-small">107 posts</p>
+        <p class="desktop-only text-small">{{ userById(post.userId).postsCount }} posts</p>
+        <p class="desktop-only text-small">{{ userById(post.userId).threadsCount }} threads</p>
 
       </div>
 
@@ -32,8 +33,6 @@
 </template>
 
 <script>
-import { findById } from '@/helpers'
-
 export default {
   name: 'PostList',
   props: {
@@ -49,7 +48,7 @@ export default {
   },
   methods: {
     userById (userId) {
-      return findById(this.users, userId)
+      return this.$store.getters.user(userId)
     }
   }
 }
