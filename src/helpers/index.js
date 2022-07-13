17f1ce50f9c276c14c283f9a3813e0ff8b7e75dc
Ian Travers <mr.itpe@gmail.com>
2022-07-13 11:14:15 +0300

    Creating Helper Functions (upsert || updateOrCreate) (ep. 48)

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,2 +1,12 @@
 export const findById = (resource, id) =>
   resource.find(r => r.id === id)
+
+export const upsert = (resources, resource) => {
+  const index = resources.findIndex(r => r.id === resource.id)
+
+  if (resource.id && index !== -1) {
+    resources[index] = resource
+  } else {
+    resources.push(resource)
+  }
+}
