8122850f5e94139d0ffeccd43a35eb77456f6b23
Ian Travers <mr.itpe@gmail.com>
2022-07-20 10:15:50 +0300

    Using the Server Timestamp for Post Published (VI) (ep. 67)

--- src/components/AppDate.vue
+++ src/components/AppDate.vue
@@ -1,6 +1,6 @@
 <template>
-  <span :title="humanFriendlyDate()">
-    {{ diffForHumans() }}
+  <span :title="humanFriendlyDate">
+    {{ diffForHumans }}
   </span>
 </template>
 
@@ -15,16 +15,19 @@ export default {
   name: 'AppDate',
   props: {
     timestamp: {
-      type: Number,
+      type: [Number, Object],
       required: true
     }
   },
-  methods: {
+  computed: {
+    normalizedTimestamp () {
+      return this.timestamp?.seconds || this.timestamp
+    },
     diffForHumans () {
-      return dayjs.unix(this.timestamp).fromNow()
+      return dayjs.unix(this.normalizedTimestamp).fromNow()
     },
     humanFriendlyDate () {
-      return dayjs.unix(this.timestamp).format('llll')
+      return dayjs.unix(this.normalizedTimestamp).format('llll')
     }
   }
 }
