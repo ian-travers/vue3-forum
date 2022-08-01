390bb3dfa068ab5ebacf4694aef6462ae62a885f
Ian Travers <mr.itpe@gmail.com>
2022-08-01 15:36:50 +0300

    Introducing useNotifications composable (ep. 114)

---
+++ src/components/AppNotifications.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="notification" v-for="notification in notifications" :key="notification.id">
+      {{ notification.message }}
+    </div>
+  </div>
+</template>
+
+<script>
+import useNotifications from '@/composibles/useNotifications'
+
+export default {
+  setup () {
+    const { notifications } = useNotifications()
+
+    return { notifications }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
