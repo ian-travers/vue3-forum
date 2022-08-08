552ff2420442da190431b05a354fe39730dcb670
Ian Travers <mr.itpe@gmail.com>
2022-08-08 11:43:25 +0300

    Clean profile page (ep. 139)

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -9,7 +9,6 @@
       <div class="col-7 push-top">
         <div class="profile-header">
           <span class="text-lead"><b>{{ user.username }}</b> recent activity</span>
-          <a href="#">See only started threads?</a>
         </div>
         <hr />
         <PostList :posts="user.posts" />
